f29ea7b16413d82fb20318186fcc43c89289b3f4
laidongxin <L65699192#>
2019-07-15 16:39:51 +0800

    see

--- src/main/java/com/xuexi/security/SpringBootSecurityApplication.java
+++ src/main/java/com/xuexi/security/SpringBootSecurityApplication.java
@@ -2,15 +2,24 @@ package com.xuexi.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * 1,引入security
  * 2,配置security
+ * @author laido
  */
 @SpringBootApplication
-public class SpringBootSecurityApplication {
+public class SpringBootSecurityApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringBootSecurityApplication.class);
+	}
+
 }
